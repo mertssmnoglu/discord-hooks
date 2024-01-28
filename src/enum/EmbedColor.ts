b9b16d0afdf41ee367f6e28cadb2cf1e0cb79b0b
Mert Şişmanoğlu <mert190737fb@gmail.com>
2024-01-28 04:15:14 +0300

    Feat: Add EmbedColor enum

---
+++ src/enum/EmbedColor.ts
@@ -0,0 +1,15 @@
+enum EmbedColor {
+  'Green' = 0x00ff00,
+  'Red' = 0xff0000,
+  'Blue' = 0x0000ff,
+  'Yellow' = 0xffff00,
+  'Orange' = 0xffa500,
+  'Purple' = 0x800080,
+  'Pink' = 0xffc0cb,
+  'Black' = 0x000000,
+  'White' = 0xffffff,
+  'Gray' = 0x808080,
+  'Brown' = 0x964b00,
+}
+
+export default EmbedColor
